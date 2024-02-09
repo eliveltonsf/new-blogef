6ade97d799c51ab7f79592ef5d98ca6db42223ae
Elivelton Ferreira <elivelton.seven@gmail.com>
2024-02-09 09:49:43 -0300

    feat: add Search page

---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Search = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Search</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Search;
