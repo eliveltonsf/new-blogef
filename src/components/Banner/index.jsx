d2f9d13fc724eaf44d37076864b528a9f208ade1
Elivelton Ferreira <elivelton.seven@gmail.com>
2024-02-09 09:45:10 -0300

    feat: add Banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,21 +1,20 @@
 import img2 from "../../img/02.png";
 
-const Banner = () => {
+const Banner = ({ content }) => {
   return (
     <>
       {" "}
       <section className="container">
         <div className="img-banner flex-center hidden">
-          <img src={img2} alt="" />
+          <img src={content.imageUrl} alt="" />
         </div>
         <div className="row mt-3">
-          <h6 className="color-gray mt-3 text-center">13 MAI 2023</h6>
-          <h6 className="uppercase color-primary text-center">tecnologia</h6>
-          <h2 className="text-center">PC Gamer muito caro, o que fazer?</h2>
-          <p className="mt-1 text-center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna
-            pharetra ut ac, pellentesque.
-          </p>
+          <h6 className="color-gray mt-3 text-center">{content.date}</h6>
+          <h6 className="uppercase color-primary text-center">
+            {content.category}
+          </h6>
+          <h2 className="text-center">{content.title}</h2>
+          <p className="mt-1 text-center">{content.resume}</p>
           <div className="my-3 flex-center">
             <a href="" className="link color-primary">
               Ler mais
