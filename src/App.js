5e722687a3be5a6d01911dc787c88246ab3a3ef6
Elivelton Ferreira <elivelton.seven@gmail.com>
2024-02-09 09:44:00 -0300

    feat: creating and adding routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Header from "pages/Header";
-import Home from "pages/Home";
-import Footer from "pages/Footer";
+import Paths from "paths";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
-  );
+  return <Paths />;
 }
 
 export default App;
---
+++ src/paths.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "pages/About";
+import Contact from "pages/Contact";
+import Login from "pages/Login";
+import NotFound from "pages/NotFound";
+import Post from "pages/Post";
+import Profile from "pages/Profile";
+import Search from "pages/Search";
+import Home from "pages/Home";
+
+function Paths() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Paths;
