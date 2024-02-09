9770a5e98c080ee2aa0a357cca03506088d6d267
Elivelton Ferreira <elivelton.seven@gmail.com>
2024-02-09 09:49:05 -0300

    feat: add NotFound page

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+import logo from "../../svg/blog-logo2.svg";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Header />
+      <section className="container">
+        <div className="row flex-center">
+          <img src={logo} className="icon-l" alt="" />
+        </div>
+
+        <div className="row">
+          <div className="grid-4 disappear"></div>
+          <div className="grid-4">
+            <h1 className="h0 color-primary text-center">404</h1>
+            <h5 className="text-center">Página não encontrada!</h5>
+            <p className="text-center mt-1">
+              A página que você tá procurando não existe ou foi removida. Clique
+              para voltar para o site.
+            </p>
+            <a href="#" className="btn text-center w-100 mt-3">
+              Voltar p/Home
+            </a>
+          </div>
+          <div className="grid-4 disappear"></div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default NotFound;
